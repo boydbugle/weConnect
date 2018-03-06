690fb98761a3a6b8886e4213faf655a1995eb563
boyd ndonga <boydbugle@gmail.com>
2018-03-06 22:36:29 +0300

    initial commit create file structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,5 @@
-# from flask import Flask,jsonify,abort,make_response,request,session
-# from user import User,USERS
-# import os
 from flask import Flask
-
 app = Flask(__name__,instance_relative_config=True)
-#
-# app.secret_key = os.urandom(24)
-#
-# users=User()
 
 from app import views
 
